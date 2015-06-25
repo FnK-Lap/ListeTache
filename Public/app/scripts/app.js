cca8b3a1508c15f5fabf72f7c50b33b83cc66f74
LucasAlumni <luca.95@live.fr>
2015-06-25 15:27:22 +0200

    add angular

---
+++ Public/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name projetNodeJsApp
+ * @description
+ * # projetNodeJsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('projetNodeJsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
