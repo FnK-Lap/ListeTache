01368f8e78f3773650ce29748b701d958da8e899
Franck Lapeyre <fnk.lap@gmail.com>
2015-06-25 16:26:49 +0200

    init node app

---
+++ app.js
@@ -0,0 +1,34 @@
+// Set Up = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+var express    = require('express');
+var app        = express();
+var http       = require('http');
+var server     = http.createServer(app);
+var mongoose   = require('mongoose');
+var bodyParser = require('body-parser');
+var router     = express.Router();
+var passport   = require('passport');
+
+// Passport
+
+// Configuration = = = = = = = = = = = = = = = = = = = = = = = = = = =
+var config   = require('./config/config.js');
+
+mongoose.connect('mongodb://'+ config.db.host +':'+ config.db.port +'/'+ config.db.dbname);
+
+
+// Use = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(passport.initialize());
+app.use('/api', router);
+app.use(express.static(__dirname + '/public/views'));
+
+// Routes = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+require('./app/route.js')(app, router);
+
+
+// Launch = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+server.listen(82); // Not 80 with MAMP
+console.log('Listen on port 82');
